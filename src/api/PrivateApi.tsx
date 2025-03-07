88d757807848521462e3de9aa14110e34e2d62fb
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2025-03-08 01:36:42 +0500

    update api routes

--- src/api/PrivateApi.tsx
+++ src/api/PrivateApi.tsx
@@ -1,55 +1,55 @@
 import { PrivateAxiosInstance } from "../config/axios";
 
-export const GET__PARKING_LIST = async (page, rowsPerPage) => {
+export const GET__PARKING_LIST = async (page: any, rowsPerPage: any) => {
     try {
         const response = await PrivateAxiosInstance.get(`/parking-list?page=${page}&limit=${rowsPerPage}`);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_PASSWORD = async (data) => {
+export const POST_PASSWORD = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`/update-password`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_COMPANY_PROFILE = async (data) => {
+export const POST_COMPANY_PROFILE = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`companies/update-company`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_PROFILE = async (data) => {
+export const POST_PROFILE = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`auth/update-profile`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_NOTIFICATION_PREFERENCES = async (data) => {
+export const POST_NOTIFICATION_PREFERENCES = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`notifications/update-notification-preferences`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_DISPLAY_MODE = async (data) => {
+export const POST_DISPLAY_MODE = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`settings/set-display-mode`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
@@ -58,61 +58,61 @@ export const GET_PROFILE_INFO = async () => {
     try {
         const response = await PrivateAxiosInstance.get(`settings/fetch-user-settings`);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const GET_TEAM_MEMBERS = async (page, limit, search, sort) => {
+export const GET_TEAM_MEMBERS = async (page: any, limit: any, search: any, sort: any) => {
     try {
         const response = await PrivateAxiosInstance.get(`companies/fetch-team-members?page=${page}&limit=${limit}&search=${search}&sortOrder=${sort}`);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_TEAM_MEMBER = async (data) => {
+export const POST_TEAM_MEMBER = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`companies/add-team-member`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_DELETE_TEAM_MEMBER = async (data) => {
+export const POST_DELETE_TEAM_MEMBER = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`companies/remove-team-member`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_MEMBER_STATUS = async (data) => {
+export const POST_MEMBER_STATUS = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`companies/update-member-status`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_PARKER_MEMBER_STATUS = async (data) => {
+export const POST_PARKER_MEMBER_STATUS = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`companies/update-member-status`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const GET_PARKER_MEMBERS = async (page, limit, search, sort, type) => {
+export const GET_PARKER_MEMBERS = async (page: any, limit: any, search: any, sort: any, type: any) => {
     try {
         const response = await PrivateAxiosInstance.get(`parker?page=${page}&limit=${limit}&search=${search}&sortOrder=${sort}&type=${type}`);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
@@ -121,25 +121,25 @@ export const GET_PARKING_AREAS = async () => {
     try {
         const response = await PrivateAxiosInstance.get(`companies/get-parking-areas`);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const GET_PARKING_AREAS_SLOTS = async (id) => {
+export const GET_PARKING_AREAS_SLOTS = async (id: any) => {
     try {
         const response = await PrivateAxiosInstance.get(`companies/get-parking-slots?parkingAreaId=${id}`);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_PARKING_AREAS = async (data) => {
+export const POST_PARKING_AREAS = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`companies/add-parking-area`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
@@ -148,34 +148,34 @@ export const GET_AVAILABLE_PARKING_SLOTS = async () => {
     try {
         const response = await PrivateAxiosInstance.get(`companies/get-available-parking-slots`);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_MANUAL_PARKER = async (data) => {
+export const POST_MANUAL_PARKER = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`companies/add-manual-parker`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const GET_MANUAL_PARKER = async (page, limit, search, sort) => {
+export const GET_MANUAL_PARKER = async (page: any, limit: any, search: any, sort: any) => {
     try {
         const response = await PrivateAxiosInstance.get(`companies/get-manual-parker?page=${page}&limit=${limit}&search=${search}&sortOrder=${sort}`);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
 
-export const POST_UPDATE_MANUAL_PARKER = async (data) => {
+export const POST_UPDATE_MANUAL_PARKER = async (data: any) => {
     try {
         const response = await PrivateAxiosInstance.post(`companies/update-manual-parker`, data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         throw new Error(error?.response?.data?.message);
     }
 };
